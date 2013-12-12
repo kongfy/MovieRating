c7ba404793718d2aaa5c97de896cf26ed93960cd
FanYu Kong <njukongfy@gmail.com>
2013-12-12 17:05:32 +0800

    修正的余弦相似度
    RMSE: 0.4971

--- main.py
+++ main.py
@@ -45,13 +45,14 @@ def load_train_data():
 def average_rate_on_movie(movie):
     total, count = movie_rate.get(movie, (0, 0))
     if (count == 0):
-        return 3.0
+        return 0.0
     return float(total) / count
+
 
 def average_rate_by_user(user):
     total, count = user_rate.get(user, (0, 0))
     if count == 0:
-        return 3.0
+        return 0.0
     return float(total) / count
 
 def similarity_between_movies(movie_i, movie_j):
@@ -68,14 +69,17 @@ def similarity_between_movies(movie_i, movie_j):
     b = 0.0
     c = 0.0
     for user in users:
-        a += (rating_data[user][movie_i] - average_rate_i) * (rating_data[user][movie_j] - average_rate_j)
-        b += (rating_data[user][movie_i] - average_rate_i) ** 2
-        c += (rating_data[user][movie_j] - average_rate_j) ** 2
+        average_rate_u = average_rate_by_user(user)
+        a += (rating_data[user][movie_i] - average_rate_u) * (rating_data[user][movie_j] - average_rate_u)
+        b += (rating_data[user][movie_i] - average_rate_u) ** 2
+        c += (rating_data[user][movie_j] - average_rate_u) ** 2
+    if a == 0:
+        return 0.0
     if b * c == 0:
-        return 0.0
+        return 1.0
     similarity = a / math.sqrt(b * c)
-    if similarity < 0:
-        return 0.0
+    #if similarity < 0:
+        #return 0.0
     return similarity
 
 def get_w(t):
@@ -83,11 +87,7 @@ def get_w(t):
     return w
 
 def k_similar_movie(movie, k):
-    return item_relations[movie]
-    
-    #temp = item_relations[movie].items()
-    #temp = sorted(temp, key = get_w, reverse = True)
-    #return temp[:k]
+    return item_relations[movie][:k]
 
 def sort_relations():
     n = len(movie_list)
@@ -113,16 +113,18 @@ def item_based_CF():
         sys.stdout.flush()
         movie_i = movie_list[i]
         relations = {}
-        for j in xrange(0, i):
-            movie_j = movie_list[j]
-            similarty = item_relations[movie_j].get(movie_i, 0)
-            if similarty != 0:
-                relations[movie_j] = similarty
-        for j in xrange(i + 1, n):
-            movie_j = movie_list[j]
-            similarty = similarity_between_movies(movie_i, movie_j)
-            if similarty != 0:
-                relations[movie_j] = similarty 
+        if movie_userlist.get(movie_i):
+            for j in xrange(0, i):
+                movie_j = movie_list[j]
+                similarty = item_relations[movie_j].get(movie_i, 0)
+                if similarty != 0:
+                    relations[movie_j] = similarty
+            for j in xrange(i + 1, n):
+                movie_j = movie_list[j]
+                if movie_userlist.get(movie_j):
+                    similarty = similarity_between_movies(movie_i, movie_j)
+                    if similarty != 0:
+                        relations[movie_j] = similarty
         item_relations[movie_i] = relations
     sys.stdout.write('\n')
 
@@ -170,42 +172,23 @@ if __name__ == '__main__':
     global movie_list
     global item_relations
     
-    if os.path.exists('train.dat'):
-        datFile = open('train.dat', 'r')
-        movie_rate = cPickle.load(datFile)
-        user_rate = cPickle.load(datFile)
-        movie_userlist = cPickle.load(datFile)
-        rating_data = cPickle.load(datFile)
-        item_relations = cPickle.load(datFile)
-        datFile.close()
-    else:
-        user_set = set()
-        movie_set = set()
-        movie_userlist = {}
-        user_movielist = {}
-        rating_data = {}
-        movie_rate = {}
-        user_rate = {}
-        
-        print 'laoding train.txt...'
-        load_train_data()
-        
-        movie_list = list(movie_set)
-        item_relations = {}
-        
-        print 'CF...'
-        item_based_CF()
-        sort_relations()
-        
-        datFile = open('train.dat', 'w')
-        cPickle.dump(movie_rate, datFile)
-        cPickle.dump(user_rate, datFile)
-        cPickle.dump(movie_userlist, datFile)
-        cPickle.dump(rating_data, datFile)
-        cPickle.dump(item_relations, datFile)
-        datFile.close()
-
-    
+	user_set = set()
+	movie_set = set()
+	movie_userlist = {}
+	user_movielist = {}
+	rating_data = {}
+	movie_rate = {}
+	user_rate = {}
+	
+	print 'laoding train.txt...'
+	load_train_data()
+	
+	movie_list = list(movie_set)
+	item_relations = {}
+	
+	print 'CF...'
+	item_based_CF()
+	sort_relations()
     
     print 'testing...'
     f_test = open('test.txt', 'r')
